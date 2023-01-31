26be1e731106d6f0f3585ac7cdbd5ea1a710b7d7
ebogdanov <ebogdanov@msk.rsvo.ru>
2023-01-31 16:51:28 +0300

    1

--- orders/asgi.py
+++ orders/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for btbRESTapi project.
+ASGI config for orders project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'btbRESTapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orders.settings')
 
 application = get_asgi_application()
