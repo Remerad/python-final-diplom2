4cdae54b40057dd971be2017ef7efd3a9ae9164c
ebogdanov <ebogdanov@msk.rsvo.ru>
2023-01-31 16:51:47 +0300

    2

---
+++ backend/admin.py
@@ -0,0 +1,89 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User, Shop, Category, Product, ProductInfo, Parameter, ProductParameter, Order, OrderItem, \
+    Contact, ConfirmEmailToken
+
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    Панель управления пользователями
+    """
+    model = User
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'type')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'company', 'position')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    model = Shop
+    fieldsets = (
+        (None, {'fields': ('name', 'state')}),
+        ('Additional Info', {'fields': ('url', 'user'),
+                             'classes': ('collapse',)}),
+    )
+    list_display = ('name', 'state', 'url')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductInfo)
+class ProductInfoAdmin(admin.ModelAdmin):
+    model = ProductInfo
+    fieldsets = (
+        (None, {'fields': ('product', 'model', 'external_id', 'quantity')}),
+        ('Prices', {'fields': ('price', 'price_rrc')}),
+    )
+    list_display = ('product', 'external_id', 'price', 'price_rrc', 'quantity')
+    ordering = ('external_id',)
+
+
+@admin.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductParameter)
+class ProductParameterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    fields = ('user', 'state', 'contact')
+    list_display = ('user', 'created', 'state')
+    ordering = ('created',)
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ConfirmEmailToken)
+class ConfirmEmailTokenAdmin(admin.ModelAdmin):
+    list_display = ('user', 'key', 'created_at',)
